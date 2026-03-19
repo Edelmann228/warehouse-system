f598c34562f61811f7d545b8a37ea911620b9ae7
Edelmann228 <Faran17@mail.ru>
2026-03-19 19:58:11 +0300

    added logic to WriteOff.cs

--- WarehouseApi/Models/WriteOff.cs
+++ WarehouseApi/Models/WriteOff.cs
@@ -1,6 +1,12 @@
-﻿namespace WarehouseApi.Models
+﻿namespace WarehouseApi.Models;
+
+public class WriteOff
 {
-    public class WriteOff
-    {
-    }
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+    public string Reason { get; set; } = string.Empty;  // Причина списания
+    public DateTime WrittenOffAt { get; set; } = DateTime.UtcNow;
+
+    public Product Product { get; set; } = null!;
 }
