62f783a5d865619a6d546fe3d21e164c55461965
Edelmann228 <Faran17@mail.ru>
2026-03-19 19:57:33 +0300

    added logic to SupplyItem.cs

--- WarehouseApi/Models/SupplyItem.cs
+++ WarehouseApi/Models/SupplyItem.cs
@@ -1,6 +1,14 @@
-﻿namespace WarehouseApi.Models
+﻿namespace WarehouseApi.Models;
+
+public class SupplyItem
 {
-    public class SupplyItem
-    {
-    }
+    public int Id { get; set; }
+    public int SupplyId { get; set; }     // Внешний ключ на Supply
+    public int ProductId { get; set; }     // Внешний ключ на Product
+    public int Quantity { get; set; }
+    public decimal UnitPrice { get; set; }
+
+    public Supply Supply { get; set; } = null!;
+    public Product Product { get; set; } = null!;
 }
+
