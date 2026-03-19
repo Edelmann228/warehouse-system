ac35cdf9986a3cebe7b9dba3be7515ca0dc0dc58
Edelmann228 <Faran17@mail.ru>
2026-03-19 23:47:44 +0300

    added unittest

--- WarehouseApi.Tests/UnitTest1.cs
+++ WarehouseApi.Tests/UnitTest1.cs
@@ -1,10 +1,20 @@
-﻿namespace WarehouseApi.Tests;
+﻿using WarehouseApi.Models;
 
-public class UnitTest1
+namespace WarehouseApi.Tests;
+
+public class ProductTests
 {
     [Fact]
-    public void Test1()
+    public void Product_StockQuantity_DefaultIsZero()
     {
+        var p = new Product { Name = "Test", SKU = "T1", Unit = "шт" };
+        Assert.True(p.StockQuantity >= 0);
+    }
 
+    [Fact]
+    public void Product_CreatedAt_IsNotDefault()
+    {
+        var p = new Product();
+        Assert.NotEqual(default(DateTime), p.CreatedAt);
     }
 }
